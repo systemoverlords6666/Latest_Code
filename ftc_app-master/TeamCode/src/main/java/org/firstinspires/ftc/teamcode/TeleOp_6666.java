938daf8734d92cb08f3b64dd2cd673ea2018a6b8
prabhassunkara <systemoverlords6666@gmail.com>
2018-11-09 20:53:16 -0600

    deleted hs

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_6666.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_6666.java
@@ -13,19 +13,19 @@ public class TeleOp_6666 extends OpMode{
     DcMotor lm;
     DcMotor rm;
     DcMotor up;
-    Servo hs;
+
 
     double leftwheelpower;
     double rightwheelpower;
     double upmotor;
-    boolean hookservo;
+
 
     @Override
     public void init() {
         lm = hardwareMap.dcMotor.get("lm");
         rm = hardwareMap.dcMotor.get("rm");
-        up = hardwareMap.dcMotor.get("cm");
-        hs = hardwareMap.servo.get("hs");
+        up = hardwareMap.dcMotor.get("up");
+
 
 
 
@@ -37,19 +37,6 @@ public class TeleOp_6666 extends OpMode{
         leftwheelpower = gamepad1.left_stick_y;
         rightwheelpower = gamepad1.right_stick_y;
         upmotor = gamepad2.left_stick_y;
-        hookservo = gamepad2.a;
-        hookservo = gamepad2.b;
-
-
-        if (gamepad2.a)
-        {
-            hs.setPosition(1);
-        }
-
-        if (gamepad2.b)
-        {
-            hs.setPosition(0);
-        }
 
 
 
