5f58cce7ef5c7bb3866b475341ec97f3f02d5e6f
Abhinav Kayithi <systemoverlords@gmail.com>
2018-12-07 17:24:45 -0600

    plz use this code named autonomous 1208

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAutonomous.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstAutonomous.java
@@ -11,29 +11,24 @@ public class FirstAutonomous extends LinearOpMode {
 
     DcMotor lm;
     DcMotor rm;
-
+    DcMotor up;
     @Override
     public void runOpMode() throws InterruptedException{
         lm = hardwareMap.dcMotor.get("lm");
         rm = hardwareMap.dcMotor.get("rm");
+        up = hardwareMap.dcMotor.get("up");
 
         rm.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
 
+        up.setPower(.4);
+        sleep(3000);
+
         lm.setPower(.4);
         rm.setPower(.4);
-        sleep(3000);
+        sleep(5000);
 
-        lm.setPower(-.4);
-        rm.setPower(-.4);
-        sleep(3000);
 
-        lm.setPower(.4);
-        rm.setPower(-.4);
-        sleep(3000);
-
-        lm.setPower(0);
-        rm.setPower(0);
 
     }
 }
