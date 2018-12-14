75428f21654920728ab480f21a23c761dbf81d0a
Abhinav Kayithi <systemoverlords@gmail.com>
2018-12-13 18:45:57 -0600

    created some new clases

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorColor.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorColor.java
@@ -3,14 +3,25 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "SensorColorDistance")
 
 public class SensorColor extends LinearOpMode {
+
+    DcMotor lm;
+    DcMotor rm;
+    DcMotor up;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
+        lm = hardwareMap.dcMotor.get("lm");
+        rm = hardwareMap.dcMotor.get("rm");
+        up = hardwareMap.dcMotor.get("up");
 
+        rm.setDirection(DcMotor.Direction.REVERSE);
+        waitForStart();
         ColorSensor color_Sensor;
         color_Sensor = hardwareMap.colorSensor.get("color_Sensor");
 
@@ -19,13 +30,15 @@ public class SensorColor extends LinearOpMode {
         color_Sensor.red();     // Red color value
         color_Sensor.blue();    // Blue color value
         color_Sensor.green();   // Green color value
-
         color_Sensor.alpha();   // Total luminosity
         color_Sensor.argb();    // Combined color value
 
         if (color_Sensor.blue()>200)
         {
 
+            lm.setPower(.8);
+            rm.setPower(.8);
+            sleep(1000);
         }
     }
 }
